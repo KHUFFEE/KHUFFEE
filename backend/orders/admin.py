4cc4262e48392671d9dd602f0583d84797fb0e77
HunSeok Sun <xodidxkq@naver.com>
2025-01-30 19:44:26 +0900

    feat: Django 초기 설정 및 MySQL 연동 완료

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Store, User
 
-# Register your models here.
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('store_id', 'store_name')  # 테이블에 표시할 필드 추가
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('store_id', 'store_name', 'store_pw')  # User 모델도 표시

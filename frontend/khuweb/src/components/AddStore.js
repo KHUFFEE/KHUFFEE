3dea49d8c33739f9ccb82882fe11c2cda7ef54c9
HunSeok Sun <xodidxkq@naver.com>
2025-02-03 16:58:39 +0900

    feat: 환경 변수 추가

--- frontend/khuweb/src/components/AddStore.js
+++ frontend/khuweb/src/components/AddStore.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 
 const AddStore = () => {
     const [storeId, setStoreId] = useState("");

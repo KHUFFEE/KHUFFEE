990bffa936b9708b9496cc80885b9b6807e143ad
HunSeok Sun <xodidxkq@naver.com>
2025-02-05 17:15:04 +0900

    feat: Django 앱 및 모델 추가

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,18 +1,51 @@
 from django.db import models
+from accounts.models import Store
+from suppliers.models import Supplier, Item
 
-class Store(models.Model):
-    store_id = models.CharField(max_length=255, primary_key=True)
-    store_name = models.CharField(max_length=255)
-
-    class Meta:
-        managed = False  # Django가 이 테이블을 생성하지 않도록 설정
-        db_table = 'store'  # MySQL의 기존 테이블과 연결
-
-class User(models.Model):
-    store = models.OneToOneField(Store, on_delete=models.CASCADE, primary_key=True)
-    store_name = models.CharField(max_length=255, blank=True, null=True)
-    store_pw = models.CharField(max_length=255, blank=True, null=True)
+class StoreOrder(models.Model):
+    매장 = models.ForeignKey(Store, on_delete=models.CASCADE)
+    품목 = models.ForeignKey(Item, on_delete=models.CASCADE)
+    기간 = models.CharField(max_length=10)  # YYYY.MM.N
+    매장_발주량 = models.IntegerField()
 
     class Meta:
-        managed = False  # Django가 이 테이블을 관리하지 않도록 설정
-        db_table = 'user'  # MySQL의 기존 테이블과 연결
+        unique_together = ('매장', '품목', '기간')
+
+    def __str__(self):
+        return f"{self.매장} - {self.품목} ({self.기간})"
+
+class WarehouseIncoming(models.Model):
+    매장 = models.ForeignKey(Store, on_delete=models.CASCADE)
+    품목 = models.ForeignKey(Item, on_delete=models.CASCADE)
+    기간 = models.CharField(max_length=10)  # YYYY.MM.N
+    창고_입고량 = models.IntegerField()
+
+    class Meta:
+        unique_together = ('매장', '품목', '기간')
+
+    def __str__(self):
+        return f"{self.매장} - {self.품목} ({self.기간})"
+
+class WarehouseOutgoing(models.Model):
+    매장 = models.ForeignKey(Store, on_delete=models.CASCADE)
+    품목 = models.ForeignKey(Item, on_delete=models.CASCADE)
+    기간 = models.CharField(max_length=10)  # YYYY.MM.N
+    창고_출고량 = models.IntegerField()
+
+    class Meta:
+        unique_together = ('매장', '품목', '기간')
+
+    def __str__(self):
+        return f"{self.매장} - {self.품목} ({self.기간})"
+
+class WarehouseOrder(models.Model):
+    협력사 = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    품목 = models.ForeignKey(Item, on_delete=models.CASCADE)
+    기간 = models.CharField(max_length=10)  # YYYY.MM.N
+    창고_발주량 = models.IntegerField()
+
+    class Meta:
+        unique_together = ('협력사', '품목', '기간')
+
+    def __str__(self):
+        return f"{self.협력사} - {self.품목} ({self.기간})"

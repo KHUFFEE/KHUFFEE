bed8e8964948ea928f5f8233fe1a1234fa155aa7
SangYub LEE <restifm2@khu.ac.kr>
2025-02-06 09:00:26 +0900

    feat: 매장 발주 화면 구성 추가

---
+++ frontend/khuapp/app/(store)/index.tsx
@@ -0,0 +1,13 @@
+// app/(store)/index.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const StoreIndexScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Store Main Screen</Text>
+    </View>
+  );
+};
+
+export default StoreIndexScreen;
---
+++ frontend/khuapp/src/Store/index.tsx
@@ -0,0 +1,2 @@
+export { default as StoreEmployeeDashboard } from './StoreEmployeeDashboard';
+

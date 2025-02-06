85f577cd2a32fcdafb68a14fb8da04455e578319
HunSeok Sun <xodidxkq@naver.com>
2025-02-06 10:38:54 +0900

    feat: 로그인 API 구현 & REACT 로그인 페이지 구현

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,5 +5,5 @@ class Store(models.Model):
     매장명 = models.CharField(max_length=255)
     매장_비밀번호 = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.매장명
+    class Meta:
+        db_table = "매장"  # Django가 "매장" 테이블을 사용하도록 명시

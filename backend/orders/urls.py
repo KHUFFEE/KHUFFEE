442ddae5b44a7c50080f365ea4eec91b10c6162f
HunSeok Sun <xodidxkq@naver.com>
2025-02-14 17:49:48 +0900

    feat: order api 개발 & 제품 관리 수정 기능 개발

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,7 +1,8 @@
 # backend/orders/urls.py
 from django.urls import path
-from .views import StoreOrderCreateView
+from .views import StoreOrderCreateView, StoreOrderListView
 
 urlpatterns = [
-    path('store_order/', StoreOrderCreateView.as_view(), name='store-order-create'),
+    path('store_order_create/', StoreOrderCreateView.as_view(), name='store-order-create'),
+    path('store_order_list/', StoreOrderListView.as_view(), name='store-order-list'),
 ]
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
 from .serializers import StoreOrderCreateSerializer
+from .models import StoreOrder
+from .serializers import StoreOrderListSerializer
+
 
 class StoreOrderCreateView(APIView):
     def post(self, request):
@@ -11,3 +15,16 @@ class StoreOrderCreateView(APIView):
             store_order = serializer.save()
             return Response(StoreOrderCreateSerializer(store_order).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class StoreOrderListView(APIView):
+    def get(self, request):
+        # .values()를 사용하여 id를 제외한 필요한 필드만 조회합니다.
+        orders = list(
+            StoreOrder.objects.values(
+                '매장_id',  # 이 값은 외래키의 pk (Store 모델의 매장_id)로 반환됩니다.
+                '품목_id',  # 이 값은 Item 모델의 품목_id로 반환됩니다.
+                '기간',
+                '매장_발주량'
+            )
+        )
+        return Response(orders, status=status.HTTP_200_OK)

ffbb49b74a1fa5b15ecf80d7cd862aa21a0fc80c
SangYub LEE <restifm2@khu.ac.kr>
2025-02-14 19:08:13 +0900

    add: 발주상태 api 연결

--- frontend/khuapp/app/(login)/index.tsx
+++ frontend/khuapp/app/(login)/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './login';
 import StoreDashboardScreen from '../(store)/main';
-import SettingsScreen from '../(store)/SettingsScreen';
 
 export type RootStackParamList = {
   Login: undefined;
@@ -18,7 +17,6 @@ export default function App() {
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Main" component={StoreDashboardScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Settings" component={SettingsScreen} options={{ title: '설정' }} />
     </Stack.Navigator>
   );
 }

4262eff1d730c424172c829737b016085be206c5
HunSeok Sun <xodidxkq@naver.com>
2025-02-18 21:02:09 +0900

    feat: 기본 페이지 설정 추가

---
+++ frontend/khuweb/src/pages/WarehouseIncoming.js
@@ -0,0 +1,13 @@
+// frontend\khuweb\src\pages\WarehouseIncoming.js
+import React from "react";
+
+const WarehouseIncoming = () => {
+  return (
+    <div>
+      <h1>창고 입고 관리</h1>
+      {/* 필요한 컴포넌트 및 로직 추가 */}
+    </div>
+  );
+};
+
+export default WarehouseIncoming;

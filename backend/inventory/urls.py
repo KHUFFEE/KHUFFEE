7b1d0004e07b329ab214e835b777ada5685a7820
HunSeok Sun <xodidxkq@naver.com>
2025-02-23 11:35:05 +0900

    feat: store_order_update api 구현

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,8 +1,9 @@
 # backend/inventory/urls.py
 from django.urls import path
-from .views import StoreInventoryListView, WarehouseInventoryListView
+from .views import StoreInventoryListView, WarehouseInventoryListView, StoreInventoryUpdateView
 
 urlpatterns = [
     path('store/', StoreInventoryListView.as_view(), name='store-inventory-list'),
     path('warehouse/', WarehouseInventoryListView.as_view(), name='warehouse-inventory-list'),
+    path('store_order_update/', StoreInventoryUpdateView.as_view(), name='store-inventory-update'),
 ]

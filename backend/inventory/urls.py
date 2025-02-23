bd8c71274669f66022a3b03ae79b65755137ffbd
HunSeok Sun <xodidxkq@naver.com>
2025-02-23 17:00:41 +0900

    feat: StoreInventoryMonthEnd 관련 탭 및 api 구현

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,9 +1,9 @@
 # backend/inventory/urls.py
 from django.urls import path
-from .views import StoreInventoryListView, WarehouseInventoryListView, StoreInventoryUpdateView, StoreMonthEndInventoryUpdateView
-
+from .views import StoreInventoryListView, WarehouseInventoryListView, StoreInventoryUpdateView, StoreMonthEndInventoryUpdateView, StoreMonthEndInventoryListView
 urlpatterns = [
     path('store/', StoreInventoryListView.as_view(), name='store-inventory-list'),
+    path('store_monthend/', StoreMonthEndInventoryListView.as_view(), name='store-monthend'),
     path('warehouse/', WarehouseInventoryListView.as_view(), name='warehouse-inventory-list'),
     path('store_inventory_update/', StoreInventoryUpdateView.as_view(), name='store-inventory-update'),
     path('store_monthend_inventory_update/', StoreMonthEndInventoryUpdateView.as_view(), name='store-monthend-inventory-update'),

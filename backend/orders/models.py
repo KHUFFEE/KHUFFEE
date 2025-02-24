31d7e828b26e1a7488338437a43e3f7b12b553b2
HunSeok Sun <xodidxkq@naver.com>
2025-02-24 17:10:43 +0900

    chore: djagno model 수정

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -6,11 +6,12 @@ class StoreOrder(models.Model):
     매장_id = models.ForeignKey(Store, on_delete=models.PROTECT, db_column="매장_id")
     품목_id = models.ForeignKey(Item, on_delete=models.PROTECT, db_column="품목_id")
     기간 = models.CharField(max_length=10)  # YYYY.MM.N
+    회차 = models.PositiveSmallIntegerField(default=1)  # 새로 추가: 1~9 사이의 정수만 허용하도록 (DB에서 CHECK 제약조건 적용됨)
     매장_발주량 = models.IntegerField()
 
     class Meta:
         db_table = "매장_발주"
-        unique_together = ("매장_id", "품목_id", "기간")  # 복합 Primary Key 적용
+        unique_together = ("매장_id", "품목_id", "기간", "회차")
 
 
 class WarehouseIncoming(models.Model):

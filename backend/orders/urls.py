29aed2dfcd047670b9b583442b9681a520c87d43
HunSeok Sun <xodidxkq@naver.com>
2025-03-07 23:39:18 +0900

    feat: warehouse_outgoing 관련 api 개발

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,6 +1,6 @@
 # backend/orders/urls.py
 from django.urls import path
-from .views import StoreOrderCreateView, StoreOrderListView, StoreOrderUpdateView, WarehouseOrderCreateView, WarehouseOrderListView, WarehouseOrderUpdateView
+from .views import StoreOrderCreateView, StoreOrderListView, StoreOrderUpdateView,WarehouseOrderCreateView, WarehouseOrderListView, WarehouseOrderUpdateView, WarehouseOutgoingCreateView, WarehouseOutgoingListView, WarehouseOutgoingUpdateView
 
 urlpatterns = [
     path('store_order_create/', StoreOrderCreateView.as_view(), name='store-order-create'),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('warehouse_order_create/', WarehouseOrderCreateView.as_view(), name='warehouse-order-create'),
     path('warehouse_order_list/', WarehouseOrderListView.as_view(), name='warehouse-order-list'),
     path('warehouse_order_update/', WarehouseOrderUpdateView.as_view(), name='warehouse-order-update'),
+    path('warehouse_outgoing_create/', WarehouseOutgoingCreateView.as_view(), name='warehouse-outgoing-create'),
+    path('warehouse_outgoing_list/', WarehouseOutgoingListView.as_view(), name='warehouse-outgoing-list'),
+    path('warehouse_outgoing_update/', WarehouseOutgoingUpdateView.as_view(), name='warehouse-outgoing-update'),
 ]

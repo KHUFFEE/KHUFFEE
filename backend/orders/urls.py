54d451c1c9765d82ff2718a33670acfda3e608fa
HunSeok Sun <xodidxkq@naver.com>
2025-03-08 22:26:47 +0900

    feat: store_order_delete api 구축

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,11 +1,12 @@
 # backend/orders/urls.py
 from django.urls import path
-from .views import StoreOrderCreateView, StoreOrderListView, StoreOrderUpdateView,WarehouseOrderCreateView, WarehouseOrderListView, WarehouseOrderUpdateView, WarehouseOutgoingCreateView, WarehouseOutgoingListView, WarehouseOutgoingUpdateView
+from .views import StoreOrderCreateView, StoreOrderListView, StoreOrderUpdateView,WarehouseOrderCreateView, WarehouseOrderListView, WarehouseOrderUpdateView, WarehouseOutgoingCreateView, WarehouseOutgoingListView, WarehouseOutgoingUpdateView, StoreOrderDeleteView  
 
 urlpatterns = [
     path('store_order_create/', StoreOrderCreateView.as_view(), name='store-order-create'),
     path('store_order_list/', StoreOrderListView.as_view(), name='store-order-list'),
     path('store_order_update/', StoreOrderUpdateView.as_view(), name='store-order-update'),
+    path('store_order_delete/', StoreOrderDeleteView.as_view(), name='store-order-delete'),
     path('warehouse_order_create/', WarehouseOrderCreateView.as_view(), name='warehouse-order-create'),
     path('warehouse_order_list/', WarehouseOrderListView.as_view(), name='warehouse-order-list'),
     path('warehouse_order_update/', WarehouseOrderUpdateView.as_view(), name='warehouse-order-update'),

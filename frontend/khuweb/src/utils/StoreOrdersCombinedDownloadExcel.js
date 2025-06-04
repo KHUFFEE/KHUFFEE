fd19485ea39c95237e388fbbb15bd295dae662f0
HunSeok Sun <xodidxkq@naver.com>
2025-06-04 22:30:28 +0900

    chore: 협력사 열 정렬 문제 해결

--- frontend/khuweb/src/utils/StoreOrdersCombinedDownloadExcel.js
+++ frontend/khuweb/src/utils/StoreOrdersCombinedDownloadExcel.js
@@ -321,6 +321,12 @@ export const storeOrdersCombinedDownloadExcel = async ({ distinctPeriods }) => {
         const cellAddr = XLSX.utils.encode_cell({ r, c });
         if (!ws[cellAddr]) continue;
         ws[cellAddr].s.font = { name: "Arial", sz: c === 0 ? 12 : 10 };
+        if (c === 0) {
+          ws[cellAddr].s.alignment = {
+            horizontal: "center",
+            vertical: "center",
+          };
+        }
         if (c >= 2 && c < totalCols - 1) {
           ws[cellAddr].s.numFmt = '#,##0;(#,##0);"-"';
         }
